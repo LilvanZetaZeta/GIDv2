15a83db877759a87585e872d0c59fa264bcbcf5b
LilvanZetaZeta <iv.zamoranov@duocuc.cl>
2025-11-21 23:04:22 -0300

    Update SwaggerConfig.java

--- GID/src/main/java/com/example/gid/GID/config/SwaggerConfig.java
+++ GID/src/main/java/com/example/gid/GID/config/SwaggerConfig.java
@@ -10,13 +10,23 @@ import io.swagger.v3.oas.models.info.Info;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI().info(
-            new Info()
-            .title("API demo")
-            .version("0.1")
-            .description("Api de repaso")
-        );
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de GID")
+                        .version("v1")
+                        .description("API para la gestión de usuarios, productos y órdenes")
+                )
+                // Añade un requisito de seguridad global llamado "bearerAuth"
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        // Define el esquema de seguridad "bearerAuth"
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
-
 }
