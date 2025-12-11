bbbfc0a3ce5315a17d57821186591d34bb9c1268
LilvanZetaZeta <iv.zamoranov@duocuc.cl>
2025-12-11 17:05:02 -0300

    Create CustomUserDetailsService.java

---
+++ GID/src/main/java/com/example/gid/GID/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.gid.GID.service;
+
+import com.example.gid.GID.model.User; 
+import com.example.gid.GID.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuario no encontrado con email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
